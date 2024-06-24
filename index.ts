9a5b47afaeafaa29c4034b69a57fc9480c9132e4
thawzinaung <thawzinmm1999@gmail.com>
2024-06-24 22:12:36 +0630

    steup

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-serve({
+const server=Bun.serve({
+    port:3000,
     fetch(req) {
-      // something like this:
-      return new Response('Hello world!');
+        return new Response("hello world bun project beginner")
     }
-  })
-                        console.log(`Server is running on http://localhost:3000`);
+})                
+console.log(`Server is running on http://localhost:${server.port}`);
                        // server.listen(3000);
