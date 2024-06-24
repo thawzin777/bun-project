183d2f7deb9f5cf8ff97338619a936ec377f44d9
thawzinaung <thawzinmm1999@gmail.com>
2024-06-24 21:47:52 +0630

    steup

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+const server=Bun.serve({
+    fetch(req) {
+        // if (req.url === "/test") {
+        //     return {
+        //         status: 200,
+        //         headers: {"Content-Type": "text/html"},
+        //         body: `<h1>Welcome to my website!</h1>`
+        //         };
+        //         }
+        //         else{
+        //             return {
+        //                 status: 404,
+        //                 headers: {"Content-Type": "text/plain"},
+        //                 body: "Not Found"
+        //                 };
+        //                 }
+        return new Response("hello world bun project")
+             }
+      });
+                        console.log(`Server is running on http://localhost:3000`);
+                       // server.listen(3000);
